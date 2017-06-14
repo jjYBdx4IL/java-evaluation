7d32c97c7f216611535d92c5f038b5ec83db158b
jjYBdx4IL <jjYBdx4IL@github.com>
2017-06-14 19:46:20 +0200

    add another JavaParser test unit

--- src/test/java/com/github/javaparser/JavaParserTest.java
+++ src/test/java/com/github/javaparser/JavaParserTest.java
@@ -2,6 +2,11 @@ package com.github.javaparser;
 
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.GenericVisitor;
+import com.github.javaparser.ast.visitor.GenericVisitorAdapter;
+
 import java.util.Optional;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
@@ -15,7 +20,7 @@ import org.slf4j.LoggerFactory;
 public class JavaParserTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(JavaParserTest.class);
-    
+
     @Test
     public void test() {
         CompilationUnit compilationUnit = JavaParser.parse("class A { }");
@@ -30,12 +35,34 @@ public class JavaParserTest {
         LOG.debug(classA.toString());
         assertEquals(1, classA.get().getMethodsBySignature("get").size());
     }
-    
+
     @Test
     public void test3() {
         CompilationUnit compilationUnit = JavaParser.parse("class A { public int get() { return abc; } }");
         Optional<ClassOrInterfaceDeclaration> classA = compilationUnit.getClassByName("A");
         LOG.debug(classA.toString());
         assertEquals(1, classA.get().getMethodsBySignature("get").size());
+    }
+
+    @Test
+    public void testGenericVisitor() {
+        CompilationUnit compilationUnit = JavaParser
+                .parse("class A { private boolean ok; public int get() { return abc; } }");
+
+        GenericVisitor<Void, Object> visitor = new GenericVisitorAdapter<Void, Object>() {
+            @Override
+            public Void visit(MethodDeclaration n, Object arg) {
+                LOG.info("method @ " + n.getRange().get());
+                return super.visit(n, arg);
+            }
+
+            @Override
+            public Void visit(FieldDeclaration n, Object arg) {
+                LOG.info("field @ " + n.getRange().get());
+                return super.visit(n, arg);
+            }
+        };
+
+        compilationUnit.accept(visitor, null);
     }
 }
