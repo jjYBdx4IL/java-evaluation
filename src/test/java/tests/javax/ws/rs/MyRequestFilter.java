2948ae75c725a5e2793c5c474488e640c09efe86
jjYBdx4IL <jjYBdx4IL@github.com>
2017-09-08 18:44:09 +0200

    jersey filter request access

--- src/test/java/tests/javax/ws/rs/MyRequestFilter.java
+++ src/test/java/tests/javax/ws/rs/MyRequestFilter.java
@@ -5,29 +5,37 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.ext.Provider;
 
 @Provider
 public class MyRequestFilter implements ContainerRequestFilter {
 
     private static final Logger LOG = LoggerFactory.getLogger(MyRequestFilter.class);
-    
+
+    // beware of thread safety here! filters are singletons!
+    @Context
+    private HttpServletRequest httpRequest;
+
     @Override
     public void filter(ContainerRequestContext requestContext)
         throws IOException {
 
         LOG.info("" + requestContext.getUriInfo().getMatchedResources());
+        LOG.info("filter " + httpRequest);
 
-//        final SecurityContext securityContext = requestContext.getSecurityContext();
-//        if (securityContext == null ||
-//            !securityContext.isUserInRole("privileged")) {
-//
-//            requestContext.abortWith(Response
-//                .status(Response.Status.UNAUTHORIZED)
-//                .entity("User cannot access the resource.")
-//                .build());
-//        }
+        // final SecurityContext securityContext =
+        // requestContext.getSecurityContext();
+        // if (securityContext == null ||
+        // !securityContext.isUserInRole("privileged")) {
+        //
+        // requestContext.abortWith(Response
+        // .status(Response.Status.UNAUTHORIZED)
+        // .entity("User cannot access the resource.")
+        // .build());
+        // }
     }
 }
