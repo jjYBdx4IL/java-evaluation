f0a0bc69690f23407b87dbb3de136c8442ad4757
jjYBdx4IL <jjYBdx4IL@github.com>
2017-10-15 09:06:10 +0200

    tika html parser test

---
+++ src/test/java/org/apache/tika/ConversionTest.java
@@ -0,0 +1,49 @@
+package org.apache.tika;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.sax.BodyContentHandler;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+
+public class ConversionTest {
+
+    @Test
+    public void testHtmlToTextConversion() throws Exception {
+        ByteArrayInputStream bais = new ByteArrayInputStream(("<html>\n" +
+            "<head>\n" +
+            "<title> \n" +
+            " A Simple HTML Document\n" +
+            "</title>\n" +
+            "<meta name=\"robots\" content=\"noindex,noarchive\">\n" +
+            "<meta name=\"keywords\" content=\"abc,def\">\n" +
+            "</head>\n" +
+            "<body></div>\n" +
+            "<p>This is a very simple HTML document</p>\n" +
+            "<p>It only has two paragraphs</p>\n" +
+            "</body>\n" +
+            "</html>").getBytes());
+        BodyContentHandler contenthandler = new BodyContentHandler();
+        Metadata metadata = new Metadata();
+        AutoDetectParser parser = new AutoDetectParser();
+        parser.parse(bais, contenthandler, metadata, new ParseContext());
+        assertEquals("\nThis is a very simple HTML document\n" + 
+            "\n" + 
+            "It only has two paragraphs\n" + 
+            "\n", contenthandler.toString().replace("\r", ""));
+        assertEquals("A Simple HTML Document", metadata.get("title"));
+        assertEquals("A Simple HTML Document", metadata.get("dc:title"));
+        assertNull(metadata.get("title2"));
+        assertEquals("org.apache.tika.parser.DefaultParser", metadata.getValues("X-Parsed-By")[0]);
+        assertEquals("org.apache.tika.parser.html.HtmlParser", metadata.getValues("X-Parsed-By")[1]);
+        assertEquals("ISO-8859-1", metadata.get("Content-Encoding"));
+        assertEquals("text/html; charset=ISO-8859-1", metadata.get("Content-Type"));
+        assertEquals("noindex,noarchive", metadata.get("robots"));
+        assertEquals("abc,def", metadata.get("keywords"));
+    }
+}
