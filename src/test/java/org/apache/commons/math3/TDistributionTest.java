87d9b5304f388d578bd2f1bd65ec92d751bccfe2
jjYBdx4IL <jjYBdx4IL@github.com>
2019-04-16 19:22:02 +0200

    commons math distribution example

--- src/test/java/org/apache/commons/math3/TDistributionTest.java
+++ src/test/java/org/apache/commons/math3/TDistributionTest.java
@@ -11,11 +11,19 @@ public class TDistributionTest {
     
     @Test
     public void test() {
+        dist = new TDistribution(4);
+        assertEquals(2.0, dist.getNumericalVariance(), 1e-6);
+        assertEquals(0.5, dist.cumulativeProbability(0), 1e-8);
+        
         dist = new TDistribution(10);
         assertEquals(1.25, dist.getNumericalVariance(), 1e-6);
+        assertEquals(0.5, dist.cumulativeProbability(0), 1e-8);
+        assertEquals(0.880120, dist.cumulativeProbability(dist.getNumericalVariance()), 1e-6);
+        
         dist = new TDistribution(100);
         assertEquals(1.020408d, dist.getNumericalVariance(), 1e-6);
-        
+        assertEquals(0.5, dist.cumulativeProbability(0), 1e-8);
+        assertEquals(0.845001, dist.cumulativeProbability(dist.getNumericalVariance()), 1e-6);
         assertEquals(1.0, dist.cumulativeProbability(10), 1e-8);
     }
 }
